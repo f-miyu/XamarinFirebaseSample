5e397c24d0d8de259b088b2f228cf421f15d4287
f-miyu <matsushima23@gmail.com>
2018-12-04 18:15:57 +0900

    更新

---
+++ XamarinFirebaseSample/XamarinFirebaseSample.Android/Renderers/CustomCollectionViewAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AiForms.Renderers;
+using AiForms.Renderers.Droid;
+using Android.Content;
+using Android.Support.V7.Widget;
+using Xamarin.Forms;
+
+namespace XamarinFirebaseSample.Droid.Renderers
+{
+    public class CustomCollectionViewAdapter : CollectionViewAdapter
+    {
+        RecyclerView _recyclerView;
+
+        public CustomCollectionViewAdapter(Context context, AiForms.Renderers.CollectionView collectionView, RecyclerView recyclerView, ICollectionViewRenderer renderer) : base(context, collectionView, recyclerView, renderer)
+        {
+            _recyclerView = recyclerView;
+        }
+
+        protected override void InvalidateItemDecoration()
+        {
+            Device.StartTimer(TimeSpan.FromMilliseconds(250), () =>
+            {
+                _recyclerView?.Invalidate();
+                return false;
+            });
+        }
+    }
+}
---
+++ XamarinFirebaseSample/XamarinFirebaseSample.Android/Renderers/CustomGridCollectionViewRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AiForms.Renderers;
+using AiForms.Renderers.Droid;
+using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinFirebaseSample.Controls;
+using XamarinFirebaseSample.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(CustomGridCollectionView), typeof(CustomGridCollectionViewRenderer))]
+namespace XamarinFirebaseSample.Droid.Renderers
+{
+    public class CustomGridCollectionViewRenderer : GridCollectionViewRenderer
+    {
+        public CustomGridCollectionViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AiForms.Renderers.CollectionView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Adapter = new CustomCollectionViewAdapter(Context, (GridCollectionView)Element, RecyclerView, this);
+
+                RecyclerView.SetAdapter(Adapter);
+
+                Adapter.IsAttachedToWindow = IsAttached;
+            }
+        }
+    }
+}
